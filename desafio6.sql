9d3e509909e5b252f2a942f0b94697c5d0e7e72b
Alex Silva <alexcssilva@gmail.com>
2022-04-17 12:59:00 -0300

    Desafio 06 conluidoo

---
+++ desafio6.sql
@@ -0,0 +1,8 @@
+SELECT
+  MIN(p.plans_price) AS 'faturamento_minimo',
+  MAX(p.plans_price) AS 'faturamento_maximo',
+  ROUND(AVG(p.plans_price), 2) AS 'faturamento_medio',
+  ROUND(SUM(p.plans_price), 2) AS 'faturamento_total'
+FROM plans AS p
+ INNER JOIN users AS u
+  ON p.plans_id = u.plans_id
